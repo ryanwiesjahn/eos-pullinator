bc38633dace0eaaa3a127489945db9a3eb6dad00
Ryan Wiesjahn <ryanwiesjahn@gmail.com>
2018-06-17 11:46:53 -0500

    Changed BlockListCell toggling raw block data from CSS to component state.

--- src/Modules/Blocks/Views/BlockListCell.jsx
+++ src/Modules/Blocks/Views/BlockListCell.jsx
@@ -11,19 +11,33 @@ export default class BlockListCell extends Component {
         block: {}
     };
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showBlockData: false
+        };
+    }
+
+    _toggleBlockData = () => {
+        let showBlockData = !this.state.showBlockData;
+        this.setState({ showBlockData });
+    }
+
     render() {
         return (
-            <View>
+            <View onClick={this._toggleBlockData}>
                 <Border />
                 <Header>
                     <Number>{this.props.block.block_num}</Number>
                     <Hash>{this.props.block.id}</Hash>
                 </Header>
                 <Content>
-                    <HiddenCheckbox />
                     <Timestamp><Label>Timestamp:</Label> {this.props.block.timestamp}</Timestamp>
                     <Actions><Label>Action Count:</Label> CHANGE ME</Actions>
-                    <Raw>{JSON.stringify(this.props.block)}</Raw>
+                    { this.state.showBlockData &&
+                        <Raw>{JSON.stringify(this.props.block)}</Raw>
+                    }
                 </Content>
             </View>
         );
@@ -37,12 +51,6 @@ const View = styled.label`
     margin-bottom: 20px;
     overflow: hidden;
     cursor: pointer;
-`;
-
-const HiddenCheckbox = styled.input.attrs({
-    type: "checkbox"
-})`
-    display: none;
 `;
 
 const Header = styled.header`
@@ -74,18 +82,12 @@ const Content = styled.div`
     padding: 20px;
 `;
 
-const Info = styled.p`
+const Timestamp = styled.p`
     margin: 0 0 10px;
 `;
 
-const Timestamp = Info.extend`
-    
-`;
-
-const Actions = Info.extend`
-    ${HiddenCheckbox}:not(:checked) ~ & {
-        margin: 0;
-    }
+const Actions = styled.p`
+    margin: 0;
 `;
 
 const Label = styled.span`
@@ -100,15 +102,10 @@ const Raw = styled.pre`
     background: rgba(0, 0, 0, 0.05);
     color: #7B7F92;
     font-size: 0.8em;
-    margin: 0;
+    margin: 20px 0 0;
     padding: 10px 13px;
     border-radius: 5px;
     line-height: 1.5em;
     white-space: pre-wrap;
     word-wrap: break-word;
-    display: none;
-
-    ${HiddenCheckbox}:checked ~ & {
-        display: block;
-    }
 `;
